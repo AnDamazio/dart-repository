d7f333f13e5af692fa53cc074070d9394411eeff
AnDamazio <damazioandre@hotmail.com>
2022-06-29 21:02:24 -0300

    Add constructor alternative for a class

--- classes.dart
+++ classes.dart
@@ -2,14 +2,20 @@ class Person {
   String name = '';
   int age = 0;
 
-  Person(String name, int age) {
+  // Person(String name, int age) {
+  //   this.name = name;
+  //   this.age = age;
+  // }
+
+  //or
+  Person({required String name, required int age}) {
     this.name = name;
     this.age = age;
   }
 }
 
 void main() {
-  var i = Person('André Damazio', 18);
+  var i = Person(name: 'André Damazio', age: 19);
   print(i.name);
   print(i.age);
 }
